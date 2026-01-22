55a6976effd1529a23a724a2a1df17e6b50f559a
Alirabiali20 <alirabiali20@gmail.com>
2026-01-22 19:37:26 +0000

    update

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-,const text = "مرحبا بكم في منصة Ali.dz";
-const typingElement = document.getElementById("typingText");
-let index = 0;
-const speed = 4000 / text.length; // 4 ثواني
-
-function typeWriter() {
-  if (index < text.length) {
-    typingElement.textContent += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-typeWriter();
-
 const enterBtn = document.getElementById("enterBtn");
 const splash = document.getElementById("splash");
 const main = document.getElementById("mainContent");

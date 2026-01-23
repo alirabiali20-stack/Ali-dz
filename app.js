2a5dcb1d5f9fc11149f31fd000187bdb5f93eef7
Alirabiali20 <alirabiali20@gmail.com>
2026-01-23 13:53:20 +0000

    update

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+const enterBtn = document.getElementById("enterBtn");
+const splash = document.getElementById("splash");
+const main = document.getElementById("mainContent");
 
-  const text = "مرحبا بك في منصة Ali.dz";
-  const typingElement = document.getElementById("typingText");
-  const enterBtn = document.getElementById("enterBtn");
-  const splash = document.getElementById("splash");
-  const main = document.getElementById("mainContent");
+enterBtn.onclick = () => {
+  splash.style.display = "none";
+  main.style.display = "block";
+};
 
-  let index = 0;
-
-  function typeWriter() {
-    if (index < text.length) {
-      typingElement.textContent += text.charAt(index);
-      index++;
-      setTimeout(typeWriter, 100); // سرعة الكتابة
-    }
-  }
-
-  typeWriter();
-
-  enterBtn.addEventListener("click", () => {
-    splash.style.display = "none";
-    main.style.display = "block";
-  });
-
-});
+function openForm() {
+  window.open(
+    "https://docs.google.com/forms/d/e/1FAIpQLSeSw9YfPx2v7AsvP80IPg5wWf_O3wJCcSo9BJChNzrntnXwSA/viewform",
+    "_blank"
+  );
+}
